725488c29c56fa17d1e9a547ccb6588a3847525b
Vitor <vitor.rm@gmail.com>
2023-04-04 18:55:43 -0300

    add navbar

---
+++ components/commons/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+    <header class="w-full bg-black text-gray-100 body-font mb-4 shadow-sm">
+      <div class="container mx-auto flex justify-between items-center py-6 px-5">
+        <a href="#link" class="flex flex-shrink-0 title-font font-medium items-center text-gray-900 md:mb-0">
+          <svg xmlns="http://www.w3.org/2000/svg" class="w-10 h-10 text-white p-2 bg-gradient-to-br from-pink-500 to-yellow-500 rounded-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4" />
+          </svg>
+          <span class="ml-3 text-xl text-gray-100 font-semibold antialiased">Navbar 1</span>
+        </a>
+   
+        <nav class="hidden md:ml-auto md:flex flex-wrap items-center justify-center text-base tracking-wide">
+          <a href="#link" class="mr-8 hover:text-gray-400">Início</a>
+          <a href="#link" class="mr-8 hover:text-gray-400">Abordagem</a>
+          <a href="#link" class="mr-8 hover:text-gray-400">Sobre Mim</a>
+          <a href="#link" class="mr-8 hover:text-gray-400">Blog</a>
+          <a href="#link" class="mr-8 hover:text-gray-400">Contato</a>
+        </nav>
+
+        <div class="hidden sm:inline-flex ml-auto md:ml-0 mr-4 md:mr-0 cursor-pointer">
+          <svg class="h-8 w-8" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 418.135 418.135" xml:space="preserve">
+            <g>
+              <path style="fill:#7AD06D;" d="M198.929,0.242C88.5,5.5,1.356,97.466,1.691,208.02c0.102,33.672,8.231,65.454,22.571,93.536
+                L2.245,408.429c-1.191,5.781,4.023,10.843,9.766,9.483l104.723-24.811c26.905,13.402,57.125,21.143,89.108,21.631
+                c112.869,1.724,206.982-87.897,210.5-200.724C420.113,93.065,320.295-5.538,198.929,0.242z M323.886,322.197
+                c-30.669,30.669-71.446,47.559-114.818,47.559c-25.396,0-49.71-5.698-72.269-16.935l-14.584-7.265l-64.206,15.212l13.515-65.607
+                l-7.185-14.07c-11.711-22.935-17.649-47.736-17.649-73.713c0-43.373,16.89-84.149,47.559-114.819
+                c30.395-30.395,71.837-47.56,114.822-47.56C252.443,45,293.218,61.89,323.887,92.558c30.669,30.669,47.559,71.445,47.56,114.817
+                C371.446,250.361,354.281,291.803,323.886,322.197z"/>
+              <path style="fill:#7AD06D;" d="M309.712,252.351l-40.169-11.534c-5.281-1.516-10.968-0.018-14.816,3.903l-9.823,10.008
+                c-4.142,4.22-10.427,5.576-15.909,3.358c-19.002-7.69-58.974-43.23-69.182-61.007c-2.945-5.128-2.458-11.539,1.158-16.218
+                l8.576-11.095c3.36-4.347,4.069-10.185,1.847-15.21l-16.9-38.223c-4.048-9.155-15.747-11.82-23.39-5.356
+                c-11.211,9.482-24.513,23.891-26.13,39.854c-2.851,28.144,9.219,63.622,54.862,106.222c52.73,49.215,94.956,55.717,122.449,49.057
+                c15.594-3.777,28.056-18.919,35.921-31.317C323.568,266.34,319.334,255.114,309.712,252.351z"/>
+            </g>
+          </svg>
+        </div>
+
+        <button 
+          class="md:hidden rounded-md active:outline-none focus:outline-none focus:ring-2 focus:ring-inset focus:ring-yellow-500"
+          click="{() => setIsOpen(!isOpen)}"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 rounded-md text-gray-300 bg-gradient-to-br from-transparent to-transparent hover:text-white hover:from-pink-500 hover:to-yellow-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
+
+
+        <div class="w-full flex flex-col py-4 px-3 md:hidden bg-gray-900 text-base uppercase text-center font-semibold">
+          <a href="#link" class="block px-3 py-2 rounded-md text-gray-300 hover:text-white hover:bg-gray-700">Solutions</a>
+          <a href="#link" class="block px-3 py-2 rounded-md text-gray-300 hover:text-white hover:bg-gray-700">Prices</a>
+          <a href="#link" class="block px-3 py-2 rounded-md text-gray-300 hover:text-white hover:bg-gray-700">About</a>
+          <a href="#link" class="block px-3 py-2 rounded-md text-gray-300 hover:text-white hover:bg-gray-700">Contact</a>
+        </div>
+    </header>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup () {
+        
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
+
